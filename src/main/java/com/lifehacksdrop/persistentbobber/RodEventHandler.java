f36797b4849c660b45ca7797dd94c26032ece8ec
mohamedisthebesty-cmd <mohamedisthebesty@gmail.com>
2026-02-22 21:55:45 +0200

    hi

--- src/main/java/com/lifehacksdrop/persistentbobber/RodEventHandler.java
+++ src/main/java/com/lifehacksdrop/persistentbobber/RodEventHandler.java
@@ -1,6 +1,12 @@
-// Event handler for rod events
-package com.example.persistentbobber;
+package com.lifehacksdrop.persistentbobber;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 
 public class RodEventHandler {
-    // Handle events here
+    public static void spawnBobberOnFirstCast(PlayerEntity player, ItemStack rod) {
+        // your bobber logic here
+    }
 }
